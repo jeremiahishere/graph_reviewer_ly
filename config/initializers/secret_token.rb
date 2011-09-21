e503d13419eb024b9a45e265c16ddb508791aa85
jeremiahishere <jeremiah@cloudspace.com>
2011-09-21 19:00:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GraphReviewerLy::Application.config.secret_token = '0ca0ffc890f5b54cf205126b37b7845298eab820179d3505eabe5c4568a2cc4b1fdccfc928f0a0a1cc48f04d6b209e6e544013390eb856b4eff123dc197dee69'

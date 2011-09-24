b8d0d13f4d3902a3dd823c641df9a7b289523a9a
jeremiahishere <jeremiah@cloudspace.com>
2011-09-24 11:05:24 -0400

    Added a default role for new users of basic_user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,12 @@ class User < ActiveRecord::Base
   has_many :roles, :through => :role_assignments
   attr_accessible :role_ids
 
+  after_create :set_default_role
+
+  def set_default_role
+    self.roles.push(Role.find_by_name("basic_user"))
+  end
+
   def name
     self.email
   end

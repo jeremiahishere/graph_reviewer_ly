4f360ee1b584176e815c4c0eaa81d8bcfaaadbbd
jeremiahishere <jeremiah@cloudspace.com>
2011-09-27 10:25:01 -0400

    Hack to fix an issue where you were not getting redirected correctly on a cancan permissions error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   # on a cancan error, redirect to root if the user is logged in
   # if there is no user logged in, save the url and redirect to the login page
   rescue_from CanCan::AccessDenied do |exception|
-    if !current_user
+    if !current_user && request.request_uri
       session[:url_on_missing_user_session] = request.request_uri
       redirect_to new_user_session_path, :alert => exception.message
     else

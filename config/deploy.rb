21b7f09aedb4832cf3225e3f23fa01969cbc1dc2
jeremiahishere <jeremiah@cloudspace.com>
2011-10-08 13:22:26 -0400

    Added a deploy and updated gemfile.lock

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+require "bundler/capistrano"
+set :application, "graph_reviewer"
+set :repository,  "git@github.com:jeremiahishere/graph_reviewer_ly.git"
+set :deploy_to, "/srv/#{application}"
+set :rails_env, :production
+
+set :scm, :git
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+
+role :web, "jeremiahhemphill.com"                          # Your HTTP server, Apache/etc
+role :app, "jeremiahhemphill.com"                          # This may be the same as your `Web` server
+role :db,  "jeremiahhemphill.com", :primary => true # This is where Rails migrations will run
+
+namespace :deploy do
+  task :start do
+    puts "not used"
+  end
+
+  task :stop do
+    puts "not used"
+  end
+
+  task :restart do
+    #run "service apache2 restart"
+    #run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+task :symlink_repos do
+  run "ln -s /srv/#{application}/shared/repos /srv/#{application}/current/repos"
+end
+after "deploy", "symlink_repos"
+
+namespace :db do
+  task :reset do
+    run "cd /srv/#{application}/current && rake db:drop RAILS_ENV=#{rails_env} && rake db:create RAILS_ENV=#{rails_env} && rake db:migrate RAILS_ENV=#{rails_env} && rake db:seed RAILS_ENV=#{rails_env}"
+  end  
+
+  task :migrate do
+    run "cd /srv/#{application}/current && rake db:create RAILS_ENV=#{rails_env} && rake db:migrate RAILS_ENV=#{rails_env}"
+  end
+end
+after "deploy", "db:migrate"
